0ddf944f576f3933cd6745c0550c90981686816a
hochan Lee <hochan049@gmail.com>
2021-05-21 15:59:12 +0900

    [FEAT] add getUserInput, addButtonEvent

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+  addButtonEvent();
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  this.play = function(computerInputNumbers, userInputNumbers) {
+    return '결과 값 String';
+  };
+
+  function clickSubmitButton() {
+    const userInput = getUserInputNumber();
+    console.log(userInput);
+  }
+
+  function getUserInputNumber() {
+    const userInput = document.getElementById('user-input').value;
+    return userInput;
+  }
+
+  function addButtonEvent() {
+    const _$submitButton = document.getElementById('submit');
+    _$submitButton.addEventListener('click', () => clickSubmitButton());
+  }
+}
 
 new BaseballGame();
