0502d8b31d2cff958a6839956cca93e954d75427
hochan Lee <hochan049@gmail.com>
2021-05-21 16:49:48 +0900

    [FEAT] add getComputerInputNumber

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 export default function BaseballGame() {
   addSubmitButtonEvent();
 
+  /*
+   ** Play Game
+   */
+
   this.play = function(computerInputNumbers, userInputNumbers) {
     return '결과 값 String';
   };
+
+  /*
+   ** Computer Input
+   */
+
+  function getRandomSingleDigit() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  function getComputerInputNumber() {
+    let _computerInput = 0;
+
+    for (let i = 0; i < 3; i++) {
+      let _randomNumber = getRandomSingleDigit();
+      if (i === 2 && _randomNumber === 0) {
+        _randomNumber = 1;
+      }
+      _computerInput += _randomNumber * Math.pow(10, i);
+    }
+    console.log('computerInput', _computerInput);
+    return _computerInput;
+  }
 
   /*
    ** User Input
@@ -18,8 +44,8 @@ export default function BaseballGame() {
   }
 
   function getUserInputNumber() {
-    const userInput = document.getElementById('user-input').value;
-    return userInput;
+    const _userInput = document.getElementById('user-input').value;
+    return _userInput;
   }
 
   /*
@@ -32,7 +58,8 @@ export default function BaseballGame() {
     if (!checkValidInput(_userInput)) {
     }
     //Input valud
-    console.log(_userInput);
+    getComputerInputNumber();
+    console.log('final', _userInput);
   }
 
   function addSubmitButtonEvent() {
