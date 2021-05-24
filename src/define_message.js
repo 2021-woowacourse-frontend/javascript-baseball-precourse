74e022c1d818b772f43906e218df3084063590ea
jwon42 <jwon@student.42seoul.kr>
2021-05-24 14:59:33 +0900

    [FEAT] add define_message.js

---
+++ src/define_message.js
@@ -0,0 +1,8 @@
+export const BALL = '볼';
+export const STRIKE = '스트라이크';
+export const NOTHING = '낫싱';
+export const ERROR =
+  '숫자를 다시 입력하세요 (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)';
+export const CORRECT = '🎉<strong>정답을 맞추셨습니다!</strong>🎉';
+export const RESTART = '게임을 새로 시작하시겠습니까?';
+export const REATART_BTN = '게임 재시작';
